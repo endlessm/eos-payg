5d303587aeedef850098b4b3eb979c85ef83ded6
Philip Withnall <withnall@endlessm.com>
2018-03-29 12:58:55 +0100

    libhelper: Fix using HlpConfigFile with just the GResource
    If loading a HlpConfigFile with a GResource, but none of the listed
    files existing on the file system, it would hit an assertion failure.
    Fix the special case handling of the GResource so that this works.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    https://phabricator.endlessm.com/T21619

--- libhelper/config-file.c
+++ libhelper/config-file.c
@@ -370,7 +370,7 @@ hlp_config_file_get_file_for_key (HlpConfigFile  *self,
     }
 
   /* Not found? */
-  if (i >= self->n_paths)
+  if (i > self->n_paths)
     {
       key_file = NULL;
       path = NULL;
