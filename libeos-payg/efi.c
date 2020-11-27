6e1ae0ee445df3569bbedb857479f02d2255d585
Derek Foreman <derek@endlessm.com>
2020-11-27 16:53:02 -0600

    efi: Add a function to check for SecureBootOption
    This variable is non-standard and weird, but in the cases it
    exists it might be useful to query.
    
    https://phabricator.endlessm.com/T31137

--- libeos-payg/efi.c
+++ libeos-payg/efi.c
@@ -18,6 +18,8 @@
 #define EOSPAYG_GUID         "d89c3871-ae0c-4fc5-a409-dc717aee61e7"
 #define GLOBAL_VARIABLE_GUID "8be4df61-93ca-11d2-aa0d-00e098032b8c"
 
+/* Can't figure out who owns this GUID. ECS and Lenovo have both used it */
+#define SBO_VARIABLE_GUID    "955b9041-133a-4bcf-90d1-97e1693c0e30"
 #define NVM_PREFIX           "EOSPAYG_"
 
 /* Totally bogus low performance mock storage for dry runs.
@@ -472,6 +474,40 @@ eospayg_efi_secureboot_active (void)
     return FALSE;
 
   return !!content[0];
+}
+
+/* eospayg_efi_securebootoption_disabled:
+ *
+ * Check if the SecureBootOption EFI variable exists and
+ * is disabled.
+ *
+ * The oddly inverted logic is due to the fact that most
+ * systems don't have this variable at all - if it doesn't
+ * exist, we can infer nothing about the state of Secure Boot,
+ * if it does it tells us if the Secure Boot option in the
+ * BIOS is enabled or disabled.
+ *
+ * Thus, on and not existing should likely be treated in the
+ * same way by a caller, but existing and off is a red flag
+ * for PAYG enforcement.
+ *
+ * Returns: %TRUE if the variable exists and is disabled, %FALSE otherwise
+ */
+gboolean
+eospayg_efi_securebootoption_disabled (void)
+{
+  g_autofree char *tname = full_efi_name (SBO_VARIABLE_GUID, "SecureBootOption");
+  g_autofree unsigned char *content = NULL;
+  int size;
+
+  if (test_mode)
+    return FALSE;
+
+  content = efivarfs_read (tname, &size);
+  if (!content || size != 1)
+    return FALSE;
+
+  return !content[0];
 }
 
 /* eospayg_efi_var_read:
