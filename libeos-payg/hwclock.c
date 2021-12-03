e2da9c1b7db5d90978701cd2d45527b358582dfa
João Paulo Rechi Vita <jprvita@endlessos.org>
2021-12-02 21:43:41 -0500

    hwclock: Keep the RTC in UTC
    Since when payg is enforced we disable the kernel SYSTOHC functionality
    and prevent any other userspace process from accessing the RTC,
    eos-paygd effectively becomes responsive for keeping the RTC up-to-date.
    
    This code previously assumed the RTC was configured to use the local
    time, which is not actually the case -- the RTC will always use UTC on
    standalone Endless OS installations.
    
    This commit changes eos-paygd behavior to always store the UTC time in
    the RTC, and treat values read from the RTC as UTC time.
    
    https://phabricator.endlessm.com/T32733

--- libeos-payg/hwclock.c
+++ libeos-payg/hwclock.c
@@ -30,9 +30,9 @@ static int rtc_fd = -1;
 static gboolean queued = FALSE;
 static gboolean warned = FALSE;
 
-/* Sets the hardware clock to the system clock time - this
- * assumes the hardware clock is in local time and not UTC,
- * which is the default on a fresh endless install.
+/* Sets the hardware clock to the system clock time
+ * Note: this assumes the hardware clock is in UTC, which
+ * should always be the case for standalone EOS systems.
  */
 static gboolean
 payg_hwclock_update (gpointer unused)
@@ -49,7 +49,7 @@ payg_hwclock_update (gpointer unused)
   queued = FALSE;
 
   time (&now_sec);
-  localtime_r (&now_sec, &now_tm);
+  gmtime_r (&now_sec, &now_tm);
 
   /* The RTC docs indicate the third param should be
    * struct rtc_time, however hwclock-rtc.c in util-linux
@@ -131,7 +131,7 @@ payg_hwclock_init (void)
       g_warning ("Failed to read RTC: %s", g_strerror (errno));
       return FALSE;
     }
-  rtc_secs = mktime (&rtc_tm);
+  rtc_secs = timegm (&rtc_tm);
   time (&sys_secs);
   g_debug ("RTC time:        %s", asctime (&rtc_tm));
   g_debug ("system UTC time: %s", asctime (gmtime (&sys_secs)));
