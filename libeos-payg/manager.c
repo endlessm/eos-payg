endlessm/eos-payg

commit ae1af69bbea1ad64dee2545f9d3cf0748ee1192b

service: Pass current time to provider on clock jumps
When a clock jump is detected, we already need to get the current
wallclock time to calculate the jump size. Let's pass this information
to the provider, so it can better decide how to handle the jump.

Also update the "manager" provider to use the time passed by caller in
its wallclock_time_changed implementation instead of calculating again.

https://phabricator.endlessm.com/T33873

libeos-payg/manager.c
@@ -81,7 +81,8 @@ static gboolean    epg_manager_shutdown_finish (EpgProvider          *provider,
                                                 GError              **error);
 
 static void        epg_manager_wallclock_time_changed (EpgProvider *provider,
-                                                       gint64       delta);
+                                                       gint64       delta,
+                                                       gint64       now_secs);
 
 static void        internal_save_state_cb (GObject      *source_object,
                                            GAsyncResult *result,
@@ -1685,7 +1686,8 @@ epg_manager_shutdown_finish (EpgProvider   *provider,
 
 static void
 epg_manager_wallclock_time_changed (EpgProvider *provider,
-                                    gint64       delta)
+                                    gint64       delta,
+                                    gint64       now_secs)
 {
   EpgManager *self = EPG_MANAGER (provider);
 
@@ -1698,8 +1700,6 @@ epg_manager_wallclock_time_changed (EpgProvider *provider,
    * miscalculated the consumption of credit at startup. */
   if (delta > 0)
     {
-      guint64 now_secs = epg_clock_get_time (self->clock);
-
       /* FIXME: This actually extends the expiration rather than reducing it
        * but the Endless backend is basically deprecated anyway in favor of
        * Angaza.

libeos-payg/provider.c
@@ -383,7 +383,8 @@ epg_provider_shutdown_finish (EpgProvider   *self,
  */
 void
 epg_provider_wallclock_time_changed (EpgProvider   *self,
-                                     gint64         delta)
+                                     gint64         delta,
+                                     gint64         now_secs)
 {
   g_return_if_fail (EPG_IS_PROVIDER (self));
 
@@ -391,7 +392,7 @@ epg_provider_wallclock_time_changed (EpgProvider   *self,
 
   g_assert (iface->wallclock_time_changed != NULL);
 
-  return iface->wallclock_time_changed (self, delta);
+  return iface->wallclock_time_changed (self, delta, now_secs);
 }
 
 /**

libeos-payg/provider.h
@@ -50,7 +50,8 @@ struct _EpgProviderInterface
                                       GError       **error);
 
   void            (*wallclock_time_changed)  (EpgProvider *self,
-                                              gint64       delta);
+                                              gint64       delta,
+                                              gint64       now_secs);
 
   guint64         (*get_expiry_time)         (EpgProvider *self);
   gboolean        (*get_enabled)             (EpgProvider *self);
@@ -80,7 +81,8 @@ gboolean        epg_provider_shutdown_finish (EpgProvider   *self,
                                               GError       **error);
 
 void            epg_provider_wallclock_time_changed  (EpgProvider *self,
-                                                      gint64       delta);
+                                                      gint64       delta,
+                                                      gint64       now_secs);
 
 guint64         epg_provider_get_expiry_time         (EpgProvider *self);
 gboolean        epg_provider_get_enabled             (EpgProvider *self);

libeos-payg/service.c
@@ -241,7 +241,7 @@ clock_jump_cb (gpointer user_data)
   if (clock_jump_delta != 0)
     {
       g_message ("Detected system clock jump of %" G_GINT64_FORMAT " seconds", clock_jump_delta);
-      epg_provider_wallclock_time_changed (self->provider, clock_jump_delta);
+      epg_provider_wallclock_time_changed (self->provider, clock_jump_delta, clock_realtime_secs_v1);
     }
 
   self->clock_realtime_secs_v0 = clock_realtime_secs_v1;

commit c2e400b9b7f2babf07dc7983090d5fa7d9f28e51

manager: Fix the time to set_expiry_time on clock jumps
On commit ae1af69bbea1 "service: Pass current time to provider on clock
jumps" a parameter with the time of the jump was added to the
wallclock_time_changed family of functions. Its value is relative to
CLOCK_REALTIME, and in epg_manager_wallclock_time_changed we were
passing it directly to set_expiry_time, which expects a time value
relative to CLOCK_BOOTTIME. This commit fixes that issue.

Fixes: ae1af69bbea1 ("service: Pass current time to provider on clock jumps")

https://phabricator.endlessm.com/T34550

libeos-payg/manager.c
@@ -1700,11 +1700,16 @@ epg_manager_wallclock_time_changed (EpgProvider *provider,
    * miscalculated the consumption of credit at startup. */
   if (delta > 0)
     {
+      /* In wallclock_now_secs we receive the jump timestamp in terms of
+       * CLOCK_REALTIME, but set_expiry_time expects the current timestamp in
+       * terms of CLOCK_BOOTTIME, so we get the time here again. */
+      guint64 now_secs = epg_clock_get_time (self->clock);
+
       /* FIXME: This actually extends the expiration rather than reducing it
        * but the Endless backend is basically deprecated anyway in favor of
        * Angaza.
        */
-      set_expiry_time (self, NULL, FALSE, wallclock_now_secs, (guint64)delta);
+      set_expiry_time (self, NULL, FALSE, now_secs, (guint64)delta);
     }
 
   /* Kick off an asynchronous save.